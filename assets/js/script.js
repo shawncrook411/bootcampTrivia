90f890f37425fd2baa2473ee1911f52003137ddb
Shawn Crook <shawncrook411@gmail.com>
2023-09-20 20:33:23 -0600

    created endGame function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,72 @@
-var startButton = document.getElementById("start-button");
-var timerDisplay = document.getElementById("timer")
 
-console.log(timer);
+var timerDisplay = document.getElementById("timer");
+var questionCount = 0;
+var questionCountDisplay = document.getElementById("question-count")
+var startButtonDiv = document.getElementById("button-div")
+var startButton;
+var globalCounter = 10;
 
 
-var setTimer = function() {  
+
+var displayStartButton = function() {
+
+    startButton = document.createElement('button')
+    startButton.setAttribute("id", "start-button")
+    startButton.textContent = "Start!"
+    startButton.addEventListener("click", function(){
+        startGame();
+    })
+    startButtonDiv.appendChild(startButton)
+}
+
+
+
+
+var setTimer = function() {
       
-    var count = 75;    
+    var count = globalCounter;    
     var timer = setInterval(countDown, 1000);
 
     function countDown() {
         console.log(count);
-        count--;
         var secondsDisplay = (count % 60)
         if (secondsDisplay < 10)
         {
             secondsDisplay = "0" + secondsDisplay // sets a placeholder 0... 1:07 example
         }
-        timerDisplay.textContent = (Math.floor(count / 60 )) + ":" + (secondsDisplay); // displays minutes:seconds
-
+        timerDisplay.textContent = "0" + (Math.floor(count / 60 )) + ":" + (secondsDisplay) + "s"; // displays minutes:seconds
+        
         if (count <= 0)
         {
+            gameEnds();
             console.log(count);
             clearInterval(timer)
             return;
         }
+        count--;
     }
 }   
 
+var gameEnds = function () {
+    displayStartButton();
+}
 
-startButton.addEventListener("click", function() {
+var displayNextQuestion = function() {
+    questionCountDisplay.setAttribute("class", "displayBorder")
+    questionCount++;
+    questionCountDisplay.textContent = "Question #" + questionCount + "!";
+}
+
+var startGame = function() {
+    questionCount = 0;
     setTimer();
-})
+    displayNextQuestion();
+    startButton.remove()
+}
 
+
+
+displayStartButton();
 
 
 
