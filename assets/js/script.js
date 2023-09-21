4f071d43a520c2b4b1d36918f7137bb685d9bc2e
Shawn Crook <shawncrook411@gmail.com>
2023-09-20 19:11:06 -0600

    added timer function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,40 @@
-var setTimer = function() {    
+var startButton = document.getElementById("start-button");
+var timerDisplay = document.getElementById("timer")
 
-    var count = 30;    
+console.log(timer);
+
+
+var setTimer = function() {  
+      
+    var count = 75;    
     var timer = setInterval(countDown, 1000);
 
     function countDown() {
-        count--;
         console.log(count);
+        count--;
+        var secondsDisplay = (count % 60)
+        if (secondsDisplay < 10)
+        {
+            secondsDisplay = "0" + secondsDisplay // sets a placeholder 0... 1:07 example
+        }
+        timerDisplay.textContent = (Math.floor(count / 60 )) + ":" + (secondsDisplay); // displays minutes:seconds
+
         if (count <= 0)
         {
+            console.log(count);
             clearInterval(timer)
             return;
         }
     }
-
-    
 }   
+
+
+startButton.addEventListener("click", function() {
+    setTimer();
+})
+
+
+
+
+
+
